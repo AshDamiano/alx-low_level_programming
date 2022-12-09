ed5c34b8028989a50c49704b31a1f18634e09554
AshDamiano <ashleydamiano5@gmail.com>
2022-12-08 18:47:10 -0800

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry
  * Return: Always 0
  */
+
 int main(void)
 {
-        puts("\"Programming is like building a multi lingual puzzle);
+        puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
