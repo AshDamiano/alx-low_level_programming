1c0e18917e1c88aac2755159bf0cdf9980701b2d
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 20:33:39 -0800

    eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+	char low;
+
+	for (d = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
+
