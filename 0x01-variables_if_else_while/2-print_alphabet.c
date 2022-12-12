d6d544c39c857f04e737614dfda4bae500f57bfb
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 17:19:08 -0800

    mmmh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints a-z
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
