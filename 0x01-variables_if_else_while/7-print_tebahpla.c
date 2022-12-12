2afd8ef06b8e8e7949d0c6b5866477cb91bf6747
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 20:12:48 -0800

    dance

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
+
