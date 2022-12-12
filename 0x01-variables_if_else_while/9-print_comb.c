cb29e4cc0a5290d1f26bb0317f1ed9302dde425c
AshDamiano <ashleydamiano5@gmail.com>
2022-12-12 15:49:32 -0800

    no space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /*
- * main - Entry point.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -22,8 +22,6 @@ int main(void)
 	if (d != '9')
 	{
 	putchar(',');
-	putchar(',');
-	}
 	}
 	putchar('\n');
 
