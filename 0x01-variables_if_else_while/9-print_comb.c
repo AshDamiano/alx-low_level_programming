ca7ae647f2b398bc1b8cee6556b14e36a91ab385
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 21:19:28 -0800

    lolita

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/*
+ * main - Entry point.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar (d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(',');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
