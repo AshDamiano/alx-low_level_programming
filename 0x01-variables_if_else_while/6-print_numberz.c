8acbfa71f9d898f8db797e8982137a4c2772a37d
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 19:58:00 -0800

    club

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int d;
+
+	for (d = '0'; d < = '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
+
