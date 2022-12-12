9c1a75625630a0e6145df897a501f48ce33a65f5
AshDamiano <ashleydamiano5@gmail.com>
2022-12-11 18:30:08 -0800

    lol

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return:Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if  (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return(0)
+
+}
