d08fcd80eb7488607aad0b495052c03545ee4694
AshDamiano <ashleydamiano5@gmail.com>
2022-12-12 16:03:48 -0800

    jojo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
+ *
  * Description: prints all possible combinations of single-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
 
 	int d;
