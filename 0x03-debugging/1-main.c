c6525ec380e2f57a0bcd233a209aaa7f23db7f42
AshDamiano <ashleydamiano5@gmail.com>
2022-12-16 15:15:58 -0800

    nol

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
 
 	printf("infinite loop incoming :(\n");
 
-i = 0;
-/**
- *while (i < 10)
- *{
- *	putchar(i);
- *}
- */
+	i = 0;
+	/**
+	 *while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
