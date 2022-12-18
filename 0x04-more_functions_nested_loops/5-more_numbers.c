96dc9f1724a419b27bc56b522e09b937b71e1412
AshDamiano <ashleydamiano5@gmail.com>
2022-12-18 11:08:00 -0800

    ker

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
+ */
+
+void more_numbers(void)
+
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b <= 14; b++)
+	{
+	if (b > 9)
+	{
+	_putchar((b / 10) + '0');
+	}
+	_putchar((b % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
