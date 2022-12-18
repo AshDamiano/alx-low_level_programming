02b9b91f72fc0d7a25ada6415f918cf0e2440f30
AshDamiano <ashleydamiano5@gmail.com>
2022-12-18 13:28:44 -0800

    poiu

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -9,8 +9,8 @@ int main(void)
 
 {
 	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
-	return (0);
+		print_diagonal(2);
+			print_diagonal(10);
+				print_diagonal(-4);
+					return (0);
 }
