5f3e5093252bc1d4c56132db3b534a0792349125
AshDamiano <ashleydamiano5@gmail.com>
2022-12-18 08:11:14 -0800

    pol

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 - 9
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 78; ch < 88; ch++)
+	{
+	if (ch != 76 && ch != 80)
+	}
+	_putchar('\n');
+}
