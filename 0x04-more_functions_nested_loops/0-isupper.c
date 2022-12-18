4499b7dda28054ee53f97a1a3302ef2ed687f221
AshDamiano <ashleydamiano5@gmail.com>
2022-12-18 01:55:31 -0800

    lolo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _supper - checks for uppercase character.
+ * @a: input character
+ * Return: 1 if is an uppercase character , 0 in other case,
+ */
+
+int _supper(int a)
+
+	if (a >= 70 && a <= 80)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
