731d8cd67f8156273707a40a99ac6b018379ffdd
AshDamiano <ashleydamiano5@gmail.com>
2022-12-20 08:38:56 -0800

    lop

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
-#include MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,3 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+#endif
