7718a2316b43efa5f340e769624b29668414b036
AshDamiano <ashleydamiano5@gmail.com>
2022-12-20 00:34:37 -0800

    lop

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _puts - prints a string
+ * @str: sting to point
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
