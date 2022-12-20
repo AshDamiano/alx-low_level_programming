f2b3258e9bbabeecc01b8c3ba1fc10677debf2ed
AshDamiano <ashleydamiano5@gmail.com>
2022-12-20 00:42:11 -0800

    rol

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,8 @@
 #include "main.h"
 
-
-
 /**
-
  * main - check the code
-
- *
-
  * Return: Always 0.
-
  */
 
 int main(void)
@@ -18,12 +11,9 @@ int main(void)
 
     char *str;
 
-
-
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 
     _puts(str);
 
     return (0);
-
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * _puts - prints a string
