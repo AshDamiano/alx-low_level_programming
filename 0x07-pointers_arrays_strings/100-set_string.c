a53682c6791e259f86d48a83babc59fe51fa7e6e
AshDamiano <ashleydamiano5@gmail.com>
2023-01-23 07:55:32 -0800

    Prototype

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: input
+ * @to: input
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
